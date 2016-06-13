47f42f9a5b624be1f05ab16594446930d6a9e3a1
David <d.mitterer@wallstreetenglish.de>
2016-06-13 10:57:06 +0200

    marker and info working

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 angular.module('WhoHelp', ['ngMap'])
 
 
-.controller('MapController', function(NgMap) {
+.controller('MapController', function($compile, NgMap) {
 
  var vm = this;
+
  vm.types = "['address']";
  vm.positions = [];
 
-  vm.placeChanged = function() {
+
+
+
+
+ vm.placeChanged = function() {
+
     vm.place = this.getPlace();
     console.log('location', vm.place.geometry.location);
-  
-
     vm.map.setCenter(vm.place.geometry.location);
-    vm.positions = [vm.place.geometry.location.lat(), vm.place.geometry.location.lng()];
-  }
+    vm.positions.push({lat: vm.place.geometry.location.lat(), lng: vm.place.geometry.location.lng()});
+}
+
+
+vm.showDetail = function(e, position) {
+    vm.map.showInfoWindow('foo-iw', "mapMark");
+  };
 
   NgMap.getMap().then(function(map) {
     vm.map = map;
