ecee33039e0f35655e0170650eaa6605aea5798c
David <d.mitterer@wallstreetenglish.de>
2016-06-13 15:53:12 +0200

    added marker ng-repeat

--- app.js
+++ app.js
@@ -1,47 +1,72 @@
-angular.module('WhoHelp', ['ngMap'])
+var app = angular.module('WhoHelp', ['ngMap']);
 
 
-  .controller('MapController', function ($compile, NgMap) {
+app.controller('MapController', function ($compile, NgMap) {
 
-    var vm = this;
+  var vm = this;
+  vm.position = "";
 
-    vm.position = "";
+  vm.types = "['address']";;
+  vm.markID = "test"
 
-    vm.types = "['address']";;
-    vm.markID = "test"
-
-   vm.helpLocations = [
-      {
-        name: "David",
-        description: "We need help with the garden",
-        priority: "high", //high, medium, low
-        position: ["48.134519", "11.56519"]
-      }
-    ];
+ 
 
 
-    vm.placeChanged = function () {
+  vm.placeChanged = function () {
 
-      vm.place = this.getPlace();
-      console.log('location', vm.place.geometry.location);
-      vm.map.setCenter(vm.place.geometry.location);
-      vm.position = [vm.place.geometry.location.lat(), vm.place.geometry.location.lng()];
-      vm.map.showInfoWindow('addHelp-iw', 'addHelp');
+    vm.place = this.getPlace();
+    console.log('location', vm.place.geometry.location);
+    vm.map.setCenter(vm.place.geometry.location);
+    vm.position = [vm.place.geometry.location.lat(), vm.place.geometry.location.lng()];
+    vm.map.showInfoWindow('addHelp-iw', 'addHelp');
+  }
+
+
+  vm.showDetail = function (e, markID) {
+    vm.map.showInfoWindow(e, markID);
+  };
+
+  NgMap.getMap().then(function (map) {
+    vm.map = map;
+  });
+
+});
+
+
+
+app.controller('MarkerController', function () {
+
+
+ this.helpLocations = [
+
+    {
+      name: "David",
+      description: "We need help with the garden",
+      priority: "low", //high, medium, low
+      position: ["48.134519", "11.56519"],
+      id: "a"
+    },
+
+    {
+      name: "Thomas",
+      description: "our basement is flooded",
+      priority: "medium", //high, medium, low
+      position: ["48.1377276", "11.5718083"],
+      id: "b"  // 48.1377276,11.5718083,17z
+    },
+
+    {
+      name: "Thomas",
+      description: "our basement is flooded",
+      priority: "high", //high, medium, low
+      position: ["48.130273", "11.560228"],
+      id: "c"  
     }
 
-
-    vm.showDetail = function (e, markID) {
-      vm.map.showInfoWindow(e, markID);
-    };
-
-    NgMap.getMap().then(function (map) {
-      vm.map = map;
-    });
-
-  })
-
- .controller('MarkerController', [], function () {
+  ];
 
 
 
-  })
+
+});
+
