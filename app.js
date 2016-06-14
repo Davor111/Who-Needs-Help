f253c9196483d9fc14f9967350d19d3d8096f751
David <d.mitterer@wallstreetenglish.de>
2016-06-14 12:22:02 +0200

    adding help markers works now

--- app.js
+++ app.js
@@ -9,14 +9,15 @@ app.controller('MapController', function ($compile, NgMap) {
   vm.types = "['address']";;
   vm.markerID = "";
 
-  vm.placeChanged = function () {
 
+  vm.placeChanged = function () {
     vm.place = this.getPlace();
     console.log('location', vm.place.geometry.location);
     vm.map.setCenter(vm.place.geometry.location);
     vm.position = [vm.place.geometry.location.lat(), vm.place.geometry.location.lng()];
     vm.map.showInfoWindow('addHelp-iw', 'addHelp');
   };
+
 
   vm.showDetail = function (e, markID) {
     vm.map.showInfoWindow(e, markID);
@@ -35,7 +36,6 @@ app.controller('MapController', function ($compile, NgMap) {
 
 
   vm.removeMarker = function() {
-
   };
 
 
@@ -43,19 +43,29 @@ app.controller('MapController', function ($compile, NgMap) {
     vm.map = map;
   });
 
+
+
 });
 
 
 
-app.controller('MarkerController', function () {
+app.controller('MarkerController', function ($compile, NgMap) {
 
- this.helpLocations = [
+  var marker = this; 
+
+   NgMap.getMap().then(function (map) {
+    marker.map = map;
+  });
+
+  marker.helpData = {}; 
+  marker.helpLocations = [
 
     {
       name: "David",
       description: "We need help with the garden",
       priority: "low", //high, medium, low
       position: ["48.134519", "11.56519"],
+      address: "Testaddress, München",
       id: "1"
     },
 
@@ -64,6 +74,7 @@ app.controller('MarkerController', function () {
       description: "our basement is flooded",
       priority: "medium", //high, medium, low
       position: ["48.1377276", "11.5718083"],
+      address: "Testaddress, München",
       id: "2"  // 48.1377276,11.5718083,17z
     },
 
@@ -72,10 +83,22 @@ app.controller('MarkerController', function () {
       description: "There's an evil goldfish in our pond",
       priority: "high", //high, medium, low
       position: ["48.130273", "11.560228"],
+      address: "Testaddress, München",
       id: "3"  
     }
 
   ];
+
+  marker.addHelp = function(vm) {
+    marker.helpData.address = vm.place.formatted_address;
+    marker.helpData.position = [vm.place.geometry.location.lat(), vm.place.geometry.location.lng()];
+    marker.helpData.id = marker.helpLocations.length + 1;
+    marker.helpLocations.push(marker.helpData);
+
+   // vm.position = [];
+    marker.helpData = {};
+    marker.map.hideInfoWindow('addHelp-iw');
+  };
 
 
 
